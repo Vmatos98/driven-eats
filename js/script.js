df9c503473e06729ab9f0e5581cc9f39649e4fcb
Victor Matos <antoniovitomatos@gmail.com>
2022-01-27 16:40:48 -0300

    resoluçã de erro de mesclagem

--- js/script.js
+++ js/script.js
@@ -6,4 +6,20 @@ function controleDePedido(id){
         document.getElementById("prato3").style.border = "1px solid #FFFFFF";
         document.getElementById(id).style.border = "5px solid #32B72F";
     }
+<<<<<<< HEAD
 }    
+=======
+    if (id === "bebida1" || id === "bebida2" || id === "bebida3"){
+        document.getElementById("bebida1").style.border = "1px solid #FFFFFF";
+        document.getElementById("bebida2").style.border = "1px solid #FFFFFF";
+        document.getElementById("bebida3").style.border = "1px solid #FFFFFF";
+        document.getElementById(id).style.border = "5px solid #32B72F";
+    }
+    if (id === "sobremesa1" || id === "sobremesa2" || id === "sobremesa3"){
+        document.getElementById("sobremesa1").style.border = "1px solid #FFFFFF";
+        document.getElementById("sobremesa2").style.border = "1px solid #FFFFFF";
+        document.getElementById("sobremesa3").style.border = "1px solid #FFFFFF";
+        document.getElementById(id).style.border = "5px solid #32B72F";
+    }
+}
+>>>>>>> selecionar bebida e sobremesa. Adição de elementos para correção automatica
