7c96f82d80a97aeb8b299afc563a02dcc69dbd55
Victor Matos <antoniovitomatos@gmail.com>
2022-01-27 15:13:17 -0300

    Implementação do js, selecionar ítem dos pratos

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function controleDePedido(id){
+    
+    if (id === "prato1" || id === "prato2" || id === "prato3"){
+        document.getElementById("prato1").style.border = "1px solid #FFFFFF";
+        document.getElementById("prato2").style.border = "1px solid #FFFFFF";
+        document.getElementById("prato3").style.border = "1px solid #FFFFFF";
+        document.getElementById(id).style.border = "5px solid #32B72F";
+    }
+    
